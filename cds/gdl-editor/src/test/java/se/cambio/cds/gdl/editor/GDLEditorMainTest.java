a0c71fbc854b8eba94b68af511bfb3c113a5ebbf
Iago.Corbal <Iago.Corbal@24d5da94-7af6-7447-9e6c-d9f96020beb6>
2014-03-19 10:25:04 +0000

    (CDS) Test fix
    git-svn-id: https://subversion.cambio.se/PC/Standard/CDS/openehr/gdl-tools/trunk@496355 24d5da94-7af6-7447-9e6c-d9f96020beb6

--- cds/gdl-editor/src/test/java/se/cambio/cds/gdl/editor/GDLEditorMainTest.java
+++ cds/gdl-editor/src/test/java/se/cambio/cds/gdl/editor/GDLEditorMainTest.java
@@ -2,27 +2,20 @@ package se.cambio.cds.gdl.editor;
 
 import junit.framework.TestCase;
 import org.apache.log4j.Logger;
-import se.cambio.cds.controller.guide.GuideUtil;
-import se.cambio.cds.gdl.converters.drools.CompilationErrorException;
-import se.cambio.cds.gdl.converters.drools.CompilationManager;
-import se.cambio.cds.gdl.converters.drools.GDLDroolsConverter;
 import se.cambio.cds.gdl.editor.controller.GDLEditor;
 import se.cambio.cds.gdl.model.Guide;
 import se.cambio.cds.gdl.parser.GDLParser;
-import se.cambio.cds.model.guide.dto.GuideDTO;
 import se.cambio.openehr.controller.session.data.Archetypes;
 import se.cambio.openehr.controller.session.data.Templates;
+import se.cambio.openehr.controller.terminology.session.data.Terminologies;
+import se.cambio.openehr.util.ExceptionHandler;
 import se.cambio.openehr.util.IOUtils;
 import se.cambio.openehr.util.UserConfigurationManager;
-import se.cambio.openehr.util.exceptions.InternalErrorException;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
 
 
 public class GDLEditorMainTest extends TestCase {
@@ -35,10 +28,15 @@ public class GDLEditorMainTest extends TestCase {
         if (!setUpIsDone){
             //Load archetypes and templates
             try{
+                UserConfigurationManager.setParameter(UserConfigurationManager.TERMINOLOGIES_FOLDER_KW, GDLEditorMainTest.class.getClassLoader().getResource("terminologies").getPath());
+                Terminologies.loadTerminologies();
+                UserConfigurationManager.setParameter(UserConfigurationManager.ARCHETYPES_FOLDER_KW, GDLEditorMainTest.class.getClassLoader().getResource("archetypes").getPath());
                 Archetypes.loadArchetypes();
+                UserConfigurationManager.setParameter(UserConfigurationManager.TEMPLATES_FOLDER_KW, GDLEditorMainTest.class.getClassLoader().getResource("templates").getPath());
                 Templates.loadTemplates();
             }catch(Exception e){
-                fail("Exception caught loading archetypes and templates: \n"+e);
+                ExceptionHandler.handle(e);
+                fail("Exception caught loading archetypes and templates");
             }
             setUpIsDone = true;
         }
@@ -46,13 +44,7 @@ public class GDLEditorMainTest extends TestCase {
 
 
     public void testCMGuides()  throws Exception{
-        UserConfigurationManager.setParameter(UserConfigurationManager.ARCHETYPES_FOLDER_KW, GDLEditorMainTest.class.getClassLoader().getResource("archetypes").getPath());
-        Archetypes.loadArchetypes();
-        UserConfigurationManager.setParameter(UserConfigurationManager.TEMPLATES_FOLDER_KW, GDLEditorMainTest.class.getClassLoader().getResource("templates").getPath());
-        Templates.loadTemplates();
-
         UserConfigurationManager.setParameter(UserConfigurationManager.LANGUAGE,"en");
-
         File mainGuideDir = new File(GDLEditorMainTest.class.getClassLoader().getResource("guidelines").getPath());
         for (File file : mainGuideDir.listFiles()) {
             if (file.getName().endsWith(".gdl")){
@@ -65,42 +57,6 @@ public class GDLEditorMainTest extends TestCase {
                 String guideStr2 = editor.serializeCurrentGuide();
                 assertEquals(guideStr, guideStr2);
             }
-        }
-    }
-
-    public static Collection<GuideDTO> getGuideDTOs(Collection<String> paths) throws Exception{
-        Collection<GuideDTO> guideDTOs = new ArrayList<GuideDTO>();
-        for (String path : paths) {
-            guideDTOs.add(getGuideDTO(path));
-        }
-        return guideDTOs;
-    }
-
-    public static GuideDTO getGuideDTO(String path) throws Exception{
-        File file = new File(path);
-        FileInputStream fis = new FileInputStream(file);
-        InputStreamReader in = new InputStreamReader(fis, "UTF-8");
-        String guideStr = IOUtils.toString(in);
-        Guide guide = GuideUtil.parseGuide(new ByteArrayInputStream(guideStr.getBytes()));
-        byte[] compiledGuide = null;//compile(guide);
-        return new GuideDTO(
-                guide.getId(),
-                guideStr,
-                IOUtils.getBytes(guide),
-                compiledGuide,
-                true,
-                Calendar.getInstance().getTime());
-    }
-
-    public static String createDroolsGuide(Guide guide) throws InternalErrorException {
-        return new GDLDroolsConverter(guide).convertToDrools();
-    }
-
-    public static byte[] compile(Guide guide) throws InternalErrorException{
-        try {
-            return CompilationManager.compile(createDroolsGuide(guide));
-        } catch (CompilationErrorException e) {
-            throw new InternalErrorException(e);
         }
     }
 }
