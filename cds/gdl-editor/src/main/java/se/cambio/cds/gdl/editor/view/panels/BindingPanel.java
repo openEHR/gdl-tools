292a160fc0ccf472ae11f65c03728b3625adb4fc
Iago.Corbal <Iago.Corbal@24d5da94-7af6-7447-9e6c-d9f96020beb6>
2014-03-26 11:00:20 +0000

    (CDS) allow adding multiple empty bindings
    git-svn-id: https://subversion.cambio.se/PC/Standard/CDS/openehr/gdl-tools/trunk@499039 24d5da94-7af6-7447-9e6c-d9f96020beb6

--- cds/gdl-editor/src/main/java/se/cambio/cds/gdl/editor/view/panels/BindingPanel.java
+++ cds/gdl-editor/src/main/java/se/cambio/cds/gdl/editor/view/panels/BindingPanel.java
@@ -1,27 +1,6 @@
 package se.cambio.cds.gdl.editor.view.panels;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
-
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
 import org.openehr.rm.datatypes.text.CodePhrase;
-
 import se.cambio.cds.gdl.editor.controller.GDLEditor;
 import se.cambio.cds.gdl.editor.util.GDLEditorImageUtil;
 import se.cambio.cds.gdl.editor.util.GDLEditorLanguageManager;
@@ -31,9 +10,16 @@ import se.cambio.cds.gdl.editor.view.tables.BindingTable.BindingTableModel;
 import se.cambio.cds.gdl.model.Binding;
 import se.cambio.cds.gdl.model.TermBinding;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.List;
+
 public class BindingPanel  extends JPanel implements RefreshablePanel {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 1L;
     private GDLEditor _controller = null;
@@ -45,260 +31,262 @@ public class BindingPanel  extends JPanel implements RefreshablePanel {
     private JPanel buttonPanel;
 
     public BindingPanel(GDLEditor gdlEditor, String terminologyId){
-	_controller = gdlEditor;
-	_terminologyId = terminologyId;
-	init();
+        _controller = gdlEditor;
+        _terminologyId = terminologyId;
+        init();
     }
 
     public void init(){
-	this.setLayout(new BorderLayout());
-	refresh();
+        this.setLayout(new BorderLayout());
+        refresh();
     }
 
     private JScrollPane getMainScrollPanel(){
-	if (mainScrollPanel==null){
-	    mainScrollPanel = new JScrollPane();
-	    mainScrollPanel.setViewportView(getBindingTable());
-	}
-	return mainScrollPanel;
+        if (mainScrollPanel==null){
+            mainScrollPanel = new JScrollPane();
+            mainScrollPanel.setViewportView(getBindingTable());
+        }
+        return mainScrollPanel;
     }
 
     public BindingTable getBindingTable(){
-	if (bindingTable==null){
-	    bindingTable = 
-		    new BindingTable(
-			    _controller.getTermBindings().get(_terminologyId).getBindings(),
-			    _terminologyId);
-	}
-	return bindingTable;
+        if (bindingTable==null){
+            bindingTable =
+                    new BindingTable(
+                            _controller.getTermBindings().get(_terminologyId).getBindings(),
+                            _terminologyId);
+        }
+        return bindingTable;
     }
-    
+
     public void refresh(){
-	if (mainScrollPanel!=null){
-	    remove(getMainScrollPanel());
-	    mainScrollPanel = null;
-	    bindingTable = null;
-	}
-	this.add(getMainScrollPanel(), BorderLayout.CENTER);
-	this.add(getButtonPanel(), BorderLayout.WEST);
-	BindingTableModel otm = getBindingTable().getBindingTableModel();
-	TermBinding termBinding = _controller.getTermBindings().get(_terminologyId);
+        if (mainScrollPanel!=null){
+            remove(getMainScrollPanel());
+            mainScrollPanel = null;
+            bindingTable = null;
+        }
+        this.add(getMainScrollPanel(), BorderLayout.CENTER);
+        this.add(getButtonPanel(), BorderLayout.WEST);
+        BindingTableModel otm = getBindingTable().getBindingTableModel();
+        TermBinding termBinding = _controller.getTermBindings().get(_terminologyId);
 
-	if (termBinding == null || termBinding.getBindings() == null) {
-	    return;
-	}
-	Map<String, Binding> mapBind = termBinding.getBindings();
+        if (termBinding == null || termBinding.getBindings() == null) {
+            return;
+        }
+        Map<String, Binding> mapBind = termBinding.getBindings();
 
-	Set<String> gtCodes = mapBind.keySet();
+        Set<String> gtCodes = mapBind.keySet();
 
-	List<String> gtCodesList = new ArrayList<String>();
-	gtCodesList.addAll(gtCodes);
+        List<String> gtCodesList = new ArrayList<String>();
+        gtCodesList.addAll(gtCodes);
 
-	Collections.sort(gtCodesList);
+        Collections.sort(gtCodesList);
 
-	for (Iterator<String> iterator = gtCodesList.iterator(); iterator.hasNext();) {
-	    String gtCodeString = iterator.next();
-	    Binding bind = mapBind.get(gtCodeString);
-	    Vector<String> v = new Vector<String>();
-	    v.add(gtCodeString);
-	    v.add(getCodesCommaSeperated(bind));
-	    v.add(bind.getUri()!=null?bind.getUri():"");
-	    otm.addRow(v);
+        for (Iterator<String> iterator = gtCodesList.iterator(); iterator.hasNext();) {
+            String gtCodeString = iterator.next();
+            Binding bind = mapBind.get(gtCodeString);
+            Vector<String> v = new Vector<String>();
+            v.add(gtCodeString);
+            v.add(getCodesCommaSeperated(bind));
+            v.add(bind.getUri()!=null?bind.getUri():"");
+            otm.addRow(v);
 
-	}
+        }
     }
 
     private JPanel getButtonPanel(){
-	if (buttonPanel==null){
-	    buttonPanel = new JPanel();
-	    buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-	    buttonPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-	    buttonPanel.add(getAddBindingButton());
-	    buttonPanel.add(getDeleteBindingButton());
-	}
-	return buttonPanel;
+        if (buttonPanel==null){
+            buttonPanel = new JPanel();
+            buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+            buttonPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            buttonPanel.add(getAddBindingButton());
+            buttonPanel.add(getDeleteBindingButton());
+        }
+        return buttonPanel;
     }
 
     private JButton getAddBindingButton() {
-	if (addTermBtn == null) {
-	    addTermBtn = new JButton();
-	    addTermBtn.setIcon(GDLEditorImageUtil.ADD_ICON);
-	    addTermBtn.setToolTipText(GDLEditorLanguageManager.getMessage("AddBinding"));
-	    addTermBtn.setContentAreaFilled(false);
-	    addTermBtn.setPreferredSize(new Dimension(16,16));
-	    addTermBtn.setBorderPainted(false);
-	    addTermBtn.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    addTermDefinitionInModel();
-		}
-	    });
-	}
-	return addTermBtn;
+        if (addTermBtn == null) {
+            addTermBtn = new JButton();
+            addTermBtn.setIcon(GDLEditorImageUtil.ADD_ICON);
+            addTermBtn.setToolTipText(GDLEditorLanguageManager.getMessage("AddBinding"));
+            addTermBtn.setContentAreaFilled(false);
+            addTermBtn.setPreferredSize(new Dimension(16,16));
+            addTermBtn.setBorderPainted(false);
+            addTermBtn.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    addTermDefinitionInModel();
+                }
+            });
+        }
+        return addTermBtn;
     }
 
-    private void addTermDefinitionInModel() {	
-	if(validCheck(this)){
-	    Vector<String> v = new Vector<String>();
-	    v.add("");
-	    v.add("");
-	    v.add("");
-	    getBindingTable().getBindingTableModel().addRow(v);	
-	}
+    private void addTermDefinitionInModel() {
+        if(validCheck(this)){
+            Vector<String> v = new Vector<String>();
+            v.add("");
+            v.add("");
+            v.add("");
+            getBindingTable().getBindingTableModel().addRow(v);
+        }
     }
 
     private JButton getDeleteBindingButton() {
-	if (deleteBtn == null) {
-	    deleteBtn = new JButton();
-	    deleteBtn.setToolTipText(GDLEditorLanguageManager.getMessage("DeleteBinding"));
-	    deleteBtn.setIcon(GDLEditorImageUtil.DELETE_ICON);
-	    deleteBtn.setContentAreaFilled(false);
-	    deleteBtn.setPreferredSize(new Dimension(16,16));
-	    deleteBtn.setBorderPainted(false);
-	    deleteBtn.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    deleteTermDefinitionInModel();
-		}
-	    });
-	}
-	return deleteBtn;
+        if (deleteBtn == null) {
+            deleteBtn = new JButton();
+            deleteBtn.setToolTipText(GDLEditorLanguageManager.getMessage("DeleteBinding"));
+            deleteBtn.setIcon(GDLEditorImageUtil.DELETE_ICON);
+            deleteBtn.setContentAreaFilled(false);
+            deleteBtn.setPreferredSize(new Dimension(16,16));
+            deleteBtn.setBorderPainted(false);
+            deleteBtn.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    deleteTermDefinitionInModel();
+                }
+            });
+        }
+        return deleteBtn;
     }
 
     private void deleteTermDefinitionInModel() {
-	Set<String> bindingsCodes = _controller.getTermBindings().keySet();
-	if (bindingsCodes == null || bindingsCodes.size() == 0) {
-	    JOptionPane.showMessageDialog(this,
-		    GDLEditorLanguageManager.getMessage("ErrorMessageDeleteTermData"));
-	} else {
-	    BindingTableModel otm = null;
-	    int selection = JOptionPane.showConfirmDialog(this,
-		    GDLEditorLanguageManager.getMessage("DeleteTerminologyMessage"), 
-		    GDLEditorLanguageManager.getMessage("DeleteTermPopupTitle"),
-		    JOptionPane.YES_NO_OPTION);
+        Set<String> bindingsCodes = _controller.getTermBindings().keySet();
+        if (bindingsCodes == null || bindingsCodes.size() == 0) {
+            JOptionPane.showMessageDialog(this,
+                    GDLEditorLanguageManager.getMessage("ErrorMessageDeleteTermData"));
+        } else {
+            BindingTableModel otm = null;
+            int selection = JOptionPane.showConfirmDialog(this,
+                    GDLEditorLanguageManager.getMessage("DeleteTerminologyMessage"),
+                    GDLEditorLanguageManager.getMessage("DeleteTermPopupTitle"),
+                    JOptionPane.YES_NO_OPTION);
 
-	    if (selection == JOptionPane.YES_OPTION) {
-		otm = getBindingTable().getBindingTableModel();
-		int rows[] = getBindingTable()
-			.getSelectedRows();
-		if (otm != null) {
-		    if (rows.length >= 0) {
-			for (int i = rows.length - 1; i >= 0; i--) {
-			    otm.removeRow(rows[i]);
-			}
-			getBindingTable().updateResults();
-		    }
-		    otm.fireTableDataChanged();
-		}
-	    }
-	}
+            if (selection == JOptionPane.YES_OPTION) {
+                otm = getBindingTable().getBindingTableModel();
+                int rows[] = getBindingTable()
+                        .getSelectedRows();
+                if (otm != null) {
+                    if (rows.length >= 0) {
+                        for (int i = rows.length - 1; i >= 0; i--) {
+                            otm.removeRow(rows[i]);
+                        }
+                        getBindingTable().updateResults();
+                    }
+                    otm.fireTableDataChanged();
+                }
+            }
+        }
     }
 
     public boolean validCheck(BindingPanel pannel) {
 
-	List<String> emptyCellCheck = null;
-	List<String> gtdoceDuplicateCheck = null;
-	if(pannel.getBindingTable().getRowCount() == 0){
-	    return true;
-	}
-	if (pannel.getBindingTable().getCellEditor() != null) {
-	    pannel.getBindingTable().getCellEditor().stopCellEditing();
-	}
-	gtdoceDuplicateCheck = new ArrayList<String>();
-	for (int i = 0; i < pannel.getBindingTable().getRowCount(); i++) {
-	    String om = pannel.getBindingTable().getValueAt(i, 0).toString();
+        List<String> emptyCellCheck = null;
+        List<String> gtdoceDuplicateCheck = null;
+        if(pannel.getBindingTable().getRowCount() == 0){
+            return true;
+        }
+        if (pannel.getBindingTable().getCellEditor() != null) {
+            pannel.getBindingTable().getCellEditor().stopCellEditing();
+        }
+        gtdoceDuplicateCheck = new ArrayList<String>();
+        for (int i = 0; i < pannel.getBindingTable().getRowCount(); i++) {
+            String om = pannel.getBindingTable().getValueAt(i, 0).toString();
 
-	    if (om.trim().length() == 0) {
-		JOptionPane.showMessageDialog(this,
-			"Code column cannot be empty");
-		return false;
-	    }else {
-		gtdoceDuplicateCheck.add(om);
-	    }
-	}
+           /* if (om.trim().length() == 0) {
+                JOptionPane.showMessageDialog(this,
+                        "Code column cannot be empty");
+                return false;
+            }else {*/
+            if (!om.isEmpty()){
+                gtdoceDuplicateCheck.add(om);
+            }
+            //}
+        }
 
-	Set<String> set = new HashSet<String>(gtdoceDuplicateCheck);
-	if(set.size() < gtdoceDuplicateCheck.size()){
-	    JOptionPane.showMessageDialog(this,
-		    "Cannot have duplicate Codes");
-	    return false;
-	}
+        Set<String> set = new HashSet<String>(gtdoceDuplicateCheck);
+        if(set.size() < gtdoceDuplicateCheck.size()){
+            JOptionPane.showMessageDialog(this,
+                    "Cannot have duplicate Codes");
+            return false;
+        }
 
 
-	for (int i = 0; i < pannel.getBindingTable().getRowCount(); i++) {
-	    emptyCellCheck = new ArrayList<String>();
-	    for (int j = 1; j < pannel.getBindingTable().getColumnCount(); j++) {
-		if (pannel.getBindingTable().getColumnCount() >= j) {
-		    String om = pannel.getBindingTable().getValueAt(i, j)
-			    .toString();
-		    if (om.trim().length() == 0) {
-			emptyCellCheck.add(null);
-		    } else {
-			emptyCellCheck.add(om);
-		    }
-		}
-	    }
+        for (int i = 0; i < pannel.getBindingTable().getRowCount(); i++) {
+            emptyCellCheck = new ArrayList<String>();
+            for (int j = 1; j < pannel.getBindingTable().getColumnCount(); j++) {
+                if (pannel.getBindingTable().getColumnCount() >= j) {
+                    String om = pannel.getBindingTable().getValueAt(i, j)
+                            .toString();
+                    if (om.trim().length() == 0) {
+                        emptyCellCheck.add(null);
+                    } else {
+                        emptyCellCheck.add(om);
+                    }
+                }
+            }
 
-	    int nullindex = 0;
-	    for (String nullcheck : emptyCellCheck) {
-		if (nullcheck == null) {
-		    nullindex++;
-		}
-	    }
-
-	    if (nullindex > 1) {
-		JOptionPane.showMessageDialog(this,
-			"Terminology code or Uri both cannot be empty values");
-		return false;
-	    }
-	}
-	return true;
+            int nullindex = 0;
+            for (String nullcheck : emptyCellCheck) {
+                if (nullcheck == null) {
+                    nullindex++;
+                }
+            }
+            /*
+            if (nullindex > 1) {
+                JOptionPane.showMessageDialog(this,
+                        "Terminology code or Uri both cannot be empty values");
+                return false;
+            } */
+        }
+        return true;
     }
 
     private String getCodesCommaSeperated(Binding binding)
     {
-	List<CodePhrase> phraselist = binding.getCodes();
+        List<CodePhrase> phraselist = binding.getCodes();
 
-	List<CodePhrase> newList = new ArrayList<CodePhrase>();
-	if (phraselist!=null) {
-	    boolean firstIter = true;
-	    String returnString = "";
+        List<CodePhrase> newList = new ArrayList<CodePhrase>();
+        if (phraselist!=null) {
+            boolean firstIter = true;
+            String returnString = "";
 
-	    Set<String> codeSet = new HashSet<String>();
+            Set<String> codeSet = new HashSet<String>();
 
 
-	    for (CodePhrase codePhrase : phraselist) {
-		if(codeSet.add(codePhrase.getCodeString()))
-		{
-		    newList.add(codePhrase);
+            for (CodePhrase codePhrase : phraselist) {
+                if(codeSet.add(codePhrase.getCodeString()))
+                {
+                    newList.add(codePhrase);
 
-		}
-	    }
+                }
+            }
 
-	    for (Iterator<String> iterSet = codeSet.iterator();iterSet.hasNext();) {
-		if (firstIter) {
-		    returnString = iterSet.next();
-		    firstIter = false;
-		    continue;
-		}
+            for (Iterator<String> iterSet = codeSet.iterator();iterSet.hasNext();) {
+                if (firstIter) {
+                    returnString = iterSet.next();
+                    firstIter = false;
+                    continue;
+                }
 
-		returnString += " , " + iterSet.next();
-	    }
+                returnString += " , " + iterSet.next();
+            }
 
-	    binding.setCodes(newList);
+            binding.setCodes(newList);
 
-	    return returnString;
-	}
-	else
-	{
-	    return "";
-	}
+            return returnString;
+        }
+        else
+        {
+            return "";
+        }
     }
 
     public String getOwnerTabName() {
-	return _terminologyId;
+        return _terminologyId;
     }
 
     public void setOwnerTabName(String ownerTabName) {
-	this._terminologyId = ownerTabName;
+        this._terminologyId = ownerTabName;
     }
 
 
