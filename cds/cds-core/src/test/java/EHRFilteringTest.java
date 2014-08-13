3833e2030c8372d3c3f533252df766a4cc38c498
Iago Corbal <iago.corbal@cambio.se>
2014-08-13 09:48:34 +0200

    CDS-428 improving readability of test code

--- cds/cds-core/src/test/java/EHRFilteringTest.java
+++ cds/cds-core/src/test/java/EHRFilteringTest.java
@@ -24,51 +24,97 @@ import java.util.Set;
  */
 public class EHRFilteringTest extends TestCase {
 
-    public Collection<ElementInstance> generateElementInstances(){
+    private Collection<ElementInstance> generateElementInstances(){
         Collection<ElementInstance> elementInstances = new ArrayList<ElementInstance>();
         ArchetypeReference ar = new ArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
-        elementInstances.add(new ElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]", new DvCodedText("Inpatient", "local", "at0007"), ar, null, null));
+        elementInstances.add(
+                new ElementInstance(
+                        "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]",
+                        new DvCodedText("Inpatient", "local", "at0007"),
+                        ar, null, null));
         DvDateTime dvDateTime = new DvDateTime("2013-04-05T12:01:00");
-        elementInstances.add(new ElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]", dvDateTime, ar, null, null));
+        elementInstances.add(
+                new ElementInstance(
+                        "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]",
+                        dvDateTime,
+                        ar, null, null));
 
         ar = new ArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
-        elementInstances.add(new ElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]", new DvCodedText("Inpatient", "local", "at0008"), ar, null, null));
+        elementInstances.add(
+                new ElementInstance(
+                        "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]",
+                        new DvCodedText("Inpatient", "local", "at0008"),
+                        ar, null, null));
         dvDateTime = new DvDateTime("2013-06-05T12:01:00");
-        elementInstances.add(new ElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]", dvDateTime, ar, null, null));
+        elementInstances.add(
+                new ElementInstance(
+                        "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]",
+                        dvDateTime,
+                        ar, null, null));
         return elementInstances;
     }
 
 
+    private DateTime getCurrentDateTime() {
+        return new DateTime(Calendar.getInstance().getTimeInMillis());
+    }
+
     public void testEHRFilterWithOnePredicate(){
         GeneratedElementInstanceCollection geic = new GeneratedElementInstanceCollection();
         GeneratedArchetypeReference gar = new GeneratedArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
-        new PredicateGeneratedElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]", null, gar, null, OpenEHRConstUI.NULL_FLAVOUR_CODE_NO_INFO, OperatorKind.MIN);
+        new PredicateGeneratedElementInstance(
+                "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]",
+                null, gar, null,
+                OpenEHRConstUI.NULL_FLAVOUR_CODE_NO_INFO,
+                OperatorKind.MIN);
         geic.add(gar);
-        DateTime dateTime = new DateTime(Calendar.getInstance().getTimeInMillis());
         Collection<ElementInstance> elementInstances = generateElementInstances();
-        Set<ArchetypeReference> archetypeReferenceSet = EHRDataFilterUtil.filterEHRData ("testEHRId", dateTime, geic.getAllArchetypeReferencesByDomain(Domains.EHR_ID), elementInstances);
+        Set<ArchetypeReference> archetypeReferenceSet =
+                EHRDataFilterUtil.filterEHRData ("testEHRId", getCurrentDateTime(), geic.getAllArchetypeReferencesByDomain(Domains.EHR_ID), elementInstances);
+
         assertEquals(1, archetypeReferenceSet.size());
     }
 
-    public void testEHRFilterWithMultiplePredicates(){
+    public void testEHRFilterWithMultiplePredicatesSameAR(){
         GeneratedElementInstanceCollection geic = new GeneratedElementInstanceCollection();
         GeneratedArchetypeReference gar = new GeneratedArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
-        new PredicateGeneratedElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]", new DvCodedText("Inpatient", "local", "at0007"), gar, null, null, OperatorKind.EQUALITY);
-        new PredicateGeneratedElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]", null, gar, null, OpenEHRConstUI.NULL_FLAVOUR_CODE_NO_INFO, OperatorKind.MIN);
+        new PredicateGeneratedElementInstance(
+                "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]",
+                new DvCodedText("Inpatient", "local", "at0007"),
+                gar, null, null,
+                OperatorKind.EQUALITY);
+        new PredicateGeneratedElementInstance(
+                "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]",
+                null, gar, null,
+                OpenEHRConstUI.NULL_FLAVOUR_CODE_NO_INFO, OperatorKind.MIN);
         geic.add(gar);
-        DateTime dateTime = new DateTime(Calendar.getInstance().getTimeInMillis());
         Collection<ElementInstance> elementInstances = generateElementInstances();
-        Set<ArchetypeReference> archetypeReferenceSet = EHRDataFilterUtil.filterEHRData ("testEHRId", dateTime, geic.getAllArchetypeReferencesByDomain(Domains.EHR_ID), elementInstances);
-        assertEquals(2, archetypeReferenceSet.size());
+        Set<ArchetypeReference> archetypeReferenceSet =
+                EHRDataFilterUtil.filterEHRData("testEHRId", getCurrentDateTime(), geic.getAllArchetypeReferencesByDomain(Domains.EHR_ID), elementInstances);
 
-        geic = new GeneratedElementInstanceCollection();
-        gar = new GeneratedArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
-        new PredicateGeneratedElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]", new DvCodedText("Inpatient", "local", "at0007"), gar, null, null, OperatorKind.EQUALITY);
+        assertEquals(2, archetypeReferenceSet.size());
+    }
+
+    public void testEHRFilterWithMultiplePredicatesDifferentAR(){
+        GeneratedElementInstanceCollection geic = new GeneratedElementInstanceCollection();
+        GeneratedArchetypeReference gar = new GeneratedArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
+        new PredicateGeneratedElementInstance(
+                "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0006]",
+                new DvCodedText("Inpatient", "local", "at0007"),
+                gar, null, null,
+                OperatorKind.EQUALITY);
         geic.add(gar);
         gar = new GeneratedArchetypeReference(Domains.EHR_ID, "openEHR-EHR-EVALUATION.contact.v1", null);
-        new PredicateGeneratedElementInstance("openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]", null, gar, null, OpenEHRConstUI.NULL_FLAVOUR_CODE_NO_INFO, OperatorKind.MIN);
+        new PredicateGeneratedElementInstance(
+                "openEHR-EHR-EVALUATION.contact.v1/data[at0001]/items[at0003]",
+                null, gar, null,
+                OpenEHRConstUI.NULL_FLAVOUR_CODE_NO_INFO,
+                OperatorKind.MIN);
         geic.add(gar);
-        archetypeReferenceSet = EHRDataFilterUtil.filterEHRData ("testEHRId", dateTime, geic.getAllArchetypeReferencesByDomain(Domains.EHR_ID), elementInstances);
+        Collection<ElementInstance> elementInstances = generateElementInstances();
+        Set<ArchetypeReference> archetypeReferenceSet =
+                EHRDataFilterUtil.filterEHRData("testEHRId", getCurrentDateTime(), geic.getAllArchetypeReferencesByDomain(Domains.EHR_ID), elementInstances);
+
         assertEquals(2, archetypeReferenceSet.size());
     }
 
