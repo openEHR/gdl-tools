689f93003870161d5c6bfc5360e78355269f4685
Iago Corbal <iago.corbal@cambio.se>
2014-09-29 14:47:10 +0200

    Adding version number to the maven-source-plugin. Inserting new 'add' method for DSVDef object

--- cds/cds-model/src/main/java/se/cambio/cds/model/dsvdata/DSVDef.java
+++ cds/cds-model/src/main/java/se/cambio/cds/model/dsvdata/DSVDef.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class DSVDef {
 
     private String dsvId;
-    private Map<String, DSVGuideDef> dsvGuideDefMap = new HashMap<String, DSVGuideDef>();
+    private Map<String, DSVGuideDef> dsvGuideDefMap;
 
     public DSVDef(String dsvId) {
         this.dsvId = dsvId;
@@ -20,7 +20,14 @@ public class DSVDef {
         this.dsvId = dsvId;
     }
 
-    public Map<String, DSVGuideDef> getDsvGuideDefMap() {
+    public void addDSVGuideDef(DSVGuideDef dsvGuideDef){
+        getDsvGuideDefMap().put(dsvGuideDef.getGuideId(), dsvGuideDef);
+    }
+
+    public Map<String, DSVGuideDef> getDsvGuideDefMap(){
+        if (dsvGuideDefMap == null) {
+            dsvGuideDefMap = new HashMap<String, DSVGuideDef>();
+        }
         return dsvGuideDefMap;
     }
 
