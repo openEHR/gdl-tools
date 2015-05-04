da32b0a41dddecfcc4897b8a22d3381799321591
Iago Corbal <Iago.Corbal@cambio.se>
2015-05-04 14:58:53 +0200

    Adding small changes for working with archetype paths
    Former-commit-id: 95a496933487841f31bb5b055efad7c493affc43

--- openehr-utils/openehr-utils-core/src/main/java/se/cambio/openehr/util/PathUtils.java
+++ openehr-utils/openehr-utils-core/src/main/java/se/cambio/openehr/util/PathUtils.java
@@ -25,6 +25,22 @@ public class PathUtils {
         return archetypeIds;
     }
 
+    public static String getEntryArchetypeInPath(String path){
+        String[] elemetnsInPath = path.split("/");
+        for(String elementInPath: elemetnsInPath){
+            if (elementInPath.contains("[") && elementInPath.contains("]")){
+                String candidateArchetypeId = elementInPath.substring(elementInPath.indexOf("[")+1, elementInPath.indexOf("]"));
+                if (candidateArchetypeId.contains("-" + OpenEHRConst.ACTION + ".") ||  //TODO Is this reliable??
+                        candidateArchetypeId.contains("-" + OpenEHRConst.INSTRUCTION + ".") ||
+                        candidateArchetypeId.contains("-" + OpenEHRConst.OBSERVATION + ".") ||
+                        candidateArchetypeId.contains("-" + OpenEHRConst.EVALUATION + ".")){
+                    return candidateArchetypeId;
+                }
+            }
+        }
+        return null;
+    }
+
     public static String getLastArchetypeIdInPath(String path, Collection<String> validArcehtypeIds){
         List<String> archetypeIds = getArchetypeIdsInPath(path, validArcehtypeIds);
         if (!archetypeIds.isEmpty()){
