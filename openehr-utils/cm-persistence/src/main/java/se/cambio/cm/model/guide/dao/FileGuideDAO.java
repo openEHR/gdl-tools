3f7d98007b7b0772b9510aefd23bae6cc4779d29
Iago Corbal <Iago.Corbal@cambio.se>
2015-06-12 07:38:42 +0200

    Adding annotations to File DAOs
    Former-commit-id: 2cd11f9763d60a4151789d61c16be5ba1dddbefd

--- openehr-utils/cm-persistence/src/main/java/se/cambio/cm/model/guide/dao/FileGuideDAO.java
+++ openehr-utils/cm-persistence/src/main/java/se/cambio/cm/model/guide/dao/FileGuideDAO.java
@@ -1,9 +1,13 @@
 package se.cambio.cm.model.guide.dao;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import se.cambio.cm.model.generic.dao.FileGenericCMElementDAO;
 import se.cambio.cm.model.guide.dto.GuideDTO;
 import se.cambio.openehr.util.UserConfigurationManager;
 
+@Component
+@Profile("cm-admin-file-dao")
 public class FileGuideDAO extends FileGenericCMElementDAO<GuideDTO> {
 
     public FileGuideDAO() {
--- openehr-utils/cm-persistence/src/main/java/se/cambio/cm/model/template/dao/FileTemplateDAO.java
+++ openehr-utils/cm-persistence/src/main/java/se/cambio/cm/model/template/dao/FileTemplateDAO.java
@@ -1,9 +1,13 @@
 package se.cambio.cm.model.template.dao;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import se.cambio.cm.model.generic.dao.FileGenericCMElementDAO;
 import se.cambio.cm.model.template.dto.TemplateDTO;
 import se.cambio.openehr.util.UserConfigurationManager;
 
+@Component
+@Profile("cm-admin-file-dao")
 public class FileTemplateDAO extends FileGenericCMElementDAO<TemplateDTO> {
 
     public FileTemplateDAO() {
