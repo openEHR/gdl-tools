6782f6ae6c3e2611f3adb9e09cd25e30c8a8941b
Manuel Palacio <manuel.palacio@cambio.se>
2016-08-12 13:53:29 +0200

    Let the configuration define the port and the scheme of the KM URL
    Former-commit-id: 7f010981d877d507925c5e6f4daaee90b4dc3037

--- openehr-utils/openehr-utils-common/src/main/java/se/cambio/openehr/util/misc/CDSConfigurationParametersManager.java
+++ openehr-utils/openehr-utils-common/src/main/java/se/cambio/openehr/util/misc/CDSConfigurationParametersManager.java
@@ -9,12 +9,10 @@ import org.springframework.core.env.Environment;
 @Configuration
 @PropertySources({
         @PropertySource(value = "classpath:default-cds-config.properties", ignoreResourceNotFound = true),
-        @PropertySource(value = "file:${CDS_CONFIG_DIR:/opt/cds-config}/cds-config.properties", ignoreResourceNotFound = true),
         @PropertySource(value = "file:conf/cds-config.properties", ignoreResourceNotFound = true),
 })
 public class CDSConfigurationParametersManager {
-    private static final String KM_SERVER_HOST = "km-server.host";
-    private static final String KM_SERVER_PORT = "km-server.port";
+    private static final String KM_SERVER_URL = "km-server.url";
     private static final String KM_SERVER_USER_LOGIN = "km-server.login";
     private static final String KM_SERVER_USER_PASSWD = "km-server.password";
     private static final String CDS_EXECUTION_TIMEOUT = "cds-execution.timeout";
@@ -28,20 +26,16 @@ public class CDSConfigurationParametersManager {
     public CDSConfigurationParametersManager() {
     }
 
-    public String getKmHost() {
-        return environment.getProperty(KM_SERVER_HOST, String.class, "localhost");
-    }
-
-    public Integer getKmPort() {
-        return environment.getProperty(KM_SERVER_PORT, Integer.class, 8080);
+    public String getKmUrl() {
+        return environment.getProperty(KM_SERVER_URL, "http://localhost:8080/km");
     }
 
     public String getKmUser() {
-        return environment.getProperty(KM_SERVER_USER_LOGIN, String.class, "km");
+        return environment.getProperty(KM_SERVER_USER_LOGIN, "km");
     }
 
     public String getKmPassword() {
-        return environment.getProperty(KM_SERVER_USER_PASSWD, String.class, "km");
+        return environment.getProperty(KM_SERVER_USER_PASSWD, "km");
     }
 
     public Long getCdsExecutionTimeOut() {
@@ -49,43 +43,15 @@ public class CDSConfigurationParametersManager {
     }
 
     public String getDbUrl() {
-        return environment.getProperty(DB_URL, String.class);
+        return environment.getProperty(DB_URL);
     }
 
     public String getDbUser() {
-        return environment.getProperty(DB_USER, String.class);
+        return environment.getProperty(DB_USER);
     }
 
     public String getDbPassword() {
-        return environment.getProperty(DB_PASSWORD, String.class);
+        return environment.getProperty(DB_PASSWORD);
     }
 
 }
-/*
- *  ***** BEGIN LICENSE BLOCK *****
- *  Version: MPL 2.0/GPL 2.0/LGPL 2.1
- *
- *  The contents of this file are subject to the Mozilla Public License Version
- *  2.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.mozilla.org/MPL/
- *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
- *
- *
- *  The Initial Developers of the Original Code are Iago Corbal and Rong Chen.
- *  Portions created by the Initial Developer are Copyright (C) 2012-2013
- *  the Initial Developer. All Rights Reserved.
- *
- *  Contributor(s):
- *
- * Software distributed under the License is distributed on an 'AS IS' basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- *  ***** END LICENSE BLOCK *****
- */
