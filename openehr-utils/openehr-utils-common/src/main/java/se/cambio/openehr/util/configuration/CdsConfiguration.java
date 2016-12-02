714b62d0fa4a2da32aadcc2947e58d3a73666f07
Iago Corbal <Iago.Corbal@cambio.se>
2016-12-02 15:46:33 +0100

    Adding also quantities to the possible date operations i.e. (.value - (.magnitude * 1,d))
    Former-commit-id: 062aeecc0717061b92dbcb729eb64e46e8490214

--- openehr-utils/openehr-utils-common/src/main/java/se/cambio/openehr/util/configuration/CdsConfiguration.java
+++ openehr-utils/openehr-utils-common/src/main/java/se/cambio/openehr/util/configuration/CdsConfiguration.java
@@ -2,9 +2,12 @@ package se.cambio.openehr.util.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import se.cambio.openehr.util.UserConfigurationManager;
 
 @Configuration
 @ComponentScan({"se.cambio"})
+@Import(UserConfigurationManager.class)
 public class CdsConfiguration {
 
 
